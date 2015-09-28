e59c3720e7ca95afaadcdf8d7f861d5ea17665d8
ingesolvoll <inge.solvoll@gmail.com>
2015-09-28 20:24:05 +0200

    Started adding socket code

--- src/clj/tdc_clj/core.clj
+++ src/clj/tdc_clj/core.clj
@@ -1,18 +1,32 @@
 (ns tdc-clj.core
-  (:require [org.httpkit.server :as httpkit]
+  (:require [org.httpkit.server :as httpkit :refer [send! with-channel on-close on-receive]]
             [compojure.core :refer [defroutes GET]]
             [clojure.repl :as repl]
             [clojure.tools.namespace.repl :as ns-repl]))
 
 (defonce app (atom nil))
 
+(defonce channels (atom #{}))
+
+(defn connect! [channel]
+  (timbre/info "channel open")
+  (swap! channels conj channel))
+
+(defn disconnect! [channel status]
+  (println "channel closed:" status)
+  (swap! channels #(remove #{channel} %)))
+
+(defn ws-handler [request]
+  (with-channel request channel
+                (connect! channel)
+                (on-close channel (partial disconnect! channel))))
+
 (defroutes application
-           (GET "/test" [] "12"))
+           (GET "/socket" request (ws-handler request)))
 
 (defn stop! [{:keys [server socket-ch]}]
   (server)
   (reset! app nil))
-
 
 (defn start! []
   (reset! app {:server    (httpkit/run-server #'application {:port 8080})
